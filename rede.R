44d1d632c139dd6951870c79d12c2a8cd0c983aa
Maria Eduarda Carvalho <57044209+auntduda@users.noreply.github.com>
2024-04-10 11:58:33 -0300

    testing new visualizations with plot

--- rede.R
+++ rede.R
@@ -31,7 +31,7 @@ relacao_bruto = purrr::map_dfr(card_membros,
 
 # Limpeza dos dados ----
 relacao_contagem = dplyr::count(relacao_bruto, colaborador_1, colaborador_2)
-relacao_filtro = dplyr::filter(relacao_contagem, n > 0)
+relacao_filtro = dplyr::filter(relacao_contagem, n > 9)
 
 relacao_binario = relacao_filtro %>% 
   dplyr::mutate(n = ifelse(n > 0, 1, 0))
@@ -44,23 +44,52 @@ matriz_colaboradores =tidyr::pivot_wider(relacao_filtro,
                                          values_fill = 0
 )
 
+
 ## Ordena matriz 
 matriz_colaboradores = matriz_colaboradores[, c('colaborador_1', as.character(matriz_colaboradores$colaborador_1))]
+matriz_colaboradores = matriz_colaboradores[, 2:30]
 
 ## Transforma em formato de matriz
 colab = data.matrix(matriz_colaboradores)
 
-## Remove coluna nao numerica (nomes dos colaboradores
-colab = colab[,-1]
+## Remove coluna nao numerica (nomes dos colaboradores)
+# colab = rbind(colab, c(-1))
+# colab = colab[,-1]
 
 ## Transforma em rede
 network = graph_from_adjacency_matrix(colab, weighted = TRUE, mode = c("undirected"))
 
-layout_grafico = layout_with_fr(network)
+layout_grafico = layout_as_star(network)
 
 # Avaliacao e Visualizacao
 ## Grafico da rede
-plot(network)
-plot(network, layout= layout_grafico)
+
+plot(
+  network,
+  layout = layout_with_dh(network),
+  vertex.size = log(colSums(colab), 1.5),
+  edge.arrow.size = 500,
+  vertex.label.cex = 0.8,
+  vertex.label.color = "black",
+  vertex.frame.color = adjustcolor("#1b1740", alpha.f = 700),
+  vertex.color = adjustcolor("blue", alpha.f = 0),
+  edge.color = adjustcolor("#893395", alpha.f = 0.7),
+  display.isolates = FALSE,
+  vertex.label = ifelse(colSums(colab) > 100 , colnames(colab), NA)
+)
+
+## Testes
+# colSums(colab)
+# 
+# contagem = relacao_contagem %>% 
+#   group_by(colaborador_1) %>% 
+#   summarise(n = sum(n)) %>% 
+#   arrange(-n)
+# 
+# ggplot(contagem, aes(x = fct_reorder(colaborador_1, n), n)) +
+#   geom_col() +
+#   coord_flip()
+
+# plot(network, layout= layout_grafico)
 
      
